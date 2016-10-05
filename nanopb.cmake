53f66c05adab9f55a24c07d23f240527c6b3815f
Ryan Kurte <ryankurte@gmail.com>
2016-10-05 13:15:59 +1300

    Added functions to assist bootstrapping nanopb to cmake file

--- nanopb.cmake
+++ nanopb.cmake
@@ -4,13 +4,24 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
 # Add project sources
-set(LIBNANOPB_SOURCES
+set(NANOPB_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/pb_common.c
     ${CMAKE_CURRENT_LIST_DIR}/pb_encode.c
     ${CMAKE_CURRENT_LIST_DIR}/pb_decode.c
 )
 
+# Register generator for application use
+set(NANOPB_GEN ${CMAKE_CURRENT_LIST_DIR}/generator/nanopb_generator.py)
+message("nanopb generator: ${NANOPB_GEN}")
+
+# Custom target to initialise nanopb generator
+add_custom_target(nanopb_init
+    COMMAND make
+    COMMENT "Initialising nanopb generator"
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/generator/proto)
+
 # Create library
-add_library(nanopb ${LIBNANOPB_SOURCES})
+add_library(nanopb ${NANOPB_SOURCES})
+add_dependencies(nanopb nanopb_init)
 
 set(OPTIONAL_LIBS nanopb ${OPTIONAL_LIBS})
